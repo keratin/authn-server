1d6dbb15f18a70c93afd65bc21c3f135533a073c
Lance Ivy <lance@cainlevy.net>
2017-05-05 10:07:35 -0700

    lint

--- authn.go
+++ authn.go
@@ -8,34 +8,34 @@ import (
 	"github.com/keratin/authn/handlers"
 )
 
-func StubHandler(w http.ResponseWriter, req *http.Request) {
+func stubHandler(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("not implemented"))
 }
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", StubHandler).Methods("GET")
+	r.HandleFunc("/", stubHandler).Methods("GET")
 
-	r.HandleFunc("/accounts", StubHandler).Methods("POST")
-	r.HandleFunc("/accounts/import", StubHandler).Methods("POST")
-	r.HandleFunc("/accounts/available", StubHandler).Methods("GET")
+	r.HandleFunc("/accounts", stubHandler).Methods("POST")
+	r.HandleFunc("/accounts/import", stubHandler).Methods("POST")
+	r.HandleFunc("/accounts/available", stubHandler).Methods("GET")
 
-	r.HandleFunc("/accounts/{account_id:[0-9]+}", StubHandler).Methods("DELETE")
-	r.HandleFunc("/accounts/{account_id:[0-9]+}/lock", StubHandler).Methods("PUT", "PATCH")
-	r.HandleFunc("/accounts/{account_id:[0-9]+}/unlock", StubHandler).Methods("PUT", "PATCH")
+	r.HandleFunc("/accounts/{account_id:[0-9]+}", stubHandler).Methods("DELETE")
+	r.HandleFunc("/accounts/{account_id:[0-9]+}/lock", stubHandler).Methods("PUT", "PATCH")
+	r.HandleFunc("/accounts/{account_id:[0-9]+}/unlock", stubHandler).Methods("PUT", "PATCH")
 
-	r.HandleFunc("/session", StubHandler).Methods("POST")
-	r.HandleFunc("/session", StubHandler).Methods("DELETE")
-	r.HandleFunc("/session/refresh", StubHandler).Methods("GET")
+	r.HandleFunc("/session", stubHandler).Methods("POST")
+	r.HandleFunc("/session", stubHandler).Methods("DELETE")
+	r.HandleFunc("/session/refresh", stubHandler).Methods("GET")
 
-	r.HandleFunc("/password", StubHandler).Methods("POST")
-	r.HandleFunc("/password/reset", StubHandler).Methods("GET")
+	r.HandleFunc("/password", stubHandler).Methods("POST")
+	r.HandleFunc("/password/reset", stubHandler).Methods("GET")
 
-	r.HandleFunc("/configuration", StubHandler).Methods("GET")
-	r.HandleFunc("/jwks", StubHandler).Methods("GET")
+	r.HandleFunc("/configuration", stubHandler).Methods("GET")
+	r.HandleFunc("/jwks", stubHandler).Methods("GET")
 
-	r.HandleFunc("/stats", StubHandler).Methods("GET")
+	r.HandleFunc("/stats", stubHandler).Methods("GET")
 	r.HandleFunc("/health", handlers.Health).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
