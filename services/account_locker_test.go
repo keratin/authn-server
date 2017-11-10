b974e23bf37da016f7a8eb1a88227056f4be356b
Lance Ivy <lance@cainlevy.net>
2017-11-09 20:39:37 -0800

    less ambiguous var name

--- services/account_locker_test.go
+++ services/account_locker_test.go
@@ -10,16 +10,16 @@ import (
 )
 
 func TestAccountLocker(t *testing.T) {
-	store := mock.NewAccountStore()
+	accountStore := mock.NewAccountStore()
 	refreshStore := mock.NewRefreshTokenStore()
 
 	t.Run("logged in account", func(t *testing.T) {
-		account, err := store.Create("loggedin@keratin.tech", []byte("password"))
+		account, err := accountStore.Create("loggedin@keratin.tech", []byte("password"))
 		require.NoError(t, err)
 		token1, err := refreshStore.Create(account.ID)
 		require.NoError(t, err)
 
-		errs := services.AccountLocker(store, refreshStore, account.ID)
+		errs := services.AccountLocker(accountStore, refreshStore, account.ID)
 		assert.Empty(t, errs)
 
 		id, err := refreshStore.Find(token1)
@@ -28,33 +28,33 @@ func TestAccountLocker(t *testing.T) {
 	})
 
 	t.Run("locked account", func(t *testing.T) {
-		account, err := store.Create("locked@keratin.tech", []byte("password"))
+		account, err := accountStore.Create("locked@keratin.tech", []byte("password"))
 		require.NoError(t, err)
-		err = store.Lock(account.ID)
+		err = accountStore.Lock(account.ID)
 		require.NoError(t, err)
 
-		errs := services.AccountLocker(store, refreshStore, account.ID)
+		errs := services.AccountLocker(accountStore, refreshStore, account.ID)
 		assert.Empty(t, errs)
 
-		acct, err := store.Find(account.ID)
+		acct, err := accountStore.Find(account.ID)
 		require.NoError(t, err)
 		assert.True(t, acct.Locked)
 	})
 
 	t.Run("unlocked account", func(t *testing.T) {
-		account, err := store.Create("unlocked@keratin.tech", []byte("password"))
+		account, err := accountStore.Create("unlocked@keratin.tech", []byte("password"))
 		require.NoError(t, err)
 
-		errs := services.AccountLocker(store, refreshStore, account.ID)
+		errs := services.AccountLocker(accountStore, refreshStore, account.ID)
 		assert.Empty(t, errs)
 
-		acct, err := store.Find(account.ID)
+		acct, err := accountStore.Find(account.ID)
 		require.NoError(t, err)
 		assert.True(t, acct.Locked)
 	})
 
 	t.Run("unknown account", func(t *testing.T) {
-		errs := services.AccountLocker(store, refreshStore, 123456789)
+		errs := services.AccountLocker(accountStore, refreshStore, 123456789)
 		assert.Equal(t, services.FieldErrors{{"account", services.ErrNotFound}}, errs)
 	})
 }
