68ba695b4202de4f2f3b4baa68d37bdf1f95d0e9
Lance Ivy <lance@cainlevy.net>
2017-11-15 20:59:34 -0800

    implement BlobStore in sqlite

---
+++ data/sqlite3/blob_store.go
@@ -0,0 +1,42 @@
+package sqlite3
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	sq3 "github.com/mattn/go-sqlite3"
+	"github.com/pkg/errors"
+)
+
+var placeholder = "generating"
+
+type BlobStore struct {
+	TTL      time.Duration
+	LockTime time.Duration
+	DB       *sqlx.DB
+}
+
+func (s *BlobStore) WLock(name string) (bool, error) {
+	_, err := s.DB.Exec("INSERT INTO blobs (name, blob, expires_at) VALUES (?, ?, ?)", name, placeholder, time.Now().Add(s.LockTime))
+	if i, ok := err.(sq3.Error); ok && i.ExtendedCode == sq3.ErrConstraintUnique {
+		return false, nil
+	}
+	return true, err
+}
+
+func (s *BlobStore) Write(name string, blob []byte) error {
+	_, err := s.DB.Exec("REPLACE INTO blobs (name, blob, expires_at) VALUES (?, ?, ?)", name, blob, time.Now().Add(s.LockTime))
+	return err
+}
+
+func (s *BlobStore) Read(name string) ([]byte, error) {
+	var blob []byte
+	err := s.DB.QueryRow("SELECT blob FROM blobs WHERE name = ? AND blob != ? AND expires_at > ?", name, placeholder, time.Now()).Scan(&blob)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	} else if err != nil {
+		return nil, errors.Wrap(err, "Get")
+	}
+	return blob, nil
+}
