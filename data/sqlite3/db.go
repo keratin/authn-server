ec8c8467e1518f65fd7d0fb62add6397bca86860
Atallah khedrane <akhedrane@users.noreply.github.com>
2018-02-18 13:09:54 +0100

    Ensure backward compatibility
    Check if path does not contains a . and assume it's a older style relative path then add old prefix and suffix

--- data/sqlite3/db.go
+++ data/sqlite3/db.go
@@ -12,6 +12,12 @@ func NewDB(env string) (*sqlx.DB, error) {
 	// https://github.com/mattn/go-sqlite3/issues/274#issuecomment-232942571
 	// enable a busy timeout for concurrent load. keep it short. the busy timeout can be harmful
 	// under sustained load, but helpful during short bursts.
+	
+	// this block used to keep backward compatibility 
+	if !strings.Contains(env, ".") {
+		env = "./"+ env +".db"
+	}
+	
 	return sqlx.Connect("sqlite3", fmt.Sprintf("%v?cache=shared&_busy_timeout=200", env))
 }
 
