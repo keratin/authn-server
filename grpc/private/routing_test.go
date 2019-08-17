5371d3d53a6612cbfaac0fb68162cd4bd1fe0f17
Mohammed Al Sahaf <msaa1990@gmail.com>
2019-08-17 18:15:04 +0300

    grpc: gateway: improve test logic of route registration

--- grpc/private/routing_test.go
+++ grpc/private/routing_test.go
@@ -2,6 +2,7 @@ package private
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/gorilla/mux"
@@ -13,7 +14,6 @@ import (
 	"golang.org/x/oauth2"
 )
 
-// corre
 var pathMethods = map[string]bool{
 	// correlated with public routes
 	"[GET]:/health":             true,
@@ -30,6 +30,7 @@ var pathMethods = map[string]bool{
 	"[GET]:/oauth/test/return":  true,
 
 	// private-only routes
+	"[GET]:/":                                       true,
 	"[GET]:/stats":                                  true,
 	"[GET]:/jwks":                                   true,
 	"[GET]:/configuration":                          true,
@@ -47,6 +48,14 @@ var pathMethods = map[string]bool{
 	"[DELETE]:/accounts/{id:[0-9]+}":                true,
 }
 
+func copyMap(src map[string]bool) map[string]bool {
+	dst := make(map[string]bool)
+	for k, v := range src {
+		dst[k] = v
+	}
+	return dst
+}
+
 func TestRegisterRoutes(t *testing.T) {
 	testOAuthProvider := oauth.NewProvider(&oauth2.Config{}, func(t *oauth2.Token) (*oauth.UserInfo, error) {
 		return &oauth.UserInfo{
@@ -54,7 +63,7 @@ func TestRegisterRoutes(t *testing.T) {
 			Email: "test@example.com",
 		}, nil
 	})
-	t.Run("App with SignupEnabled & AppPasswordResetURL & Actives enabled has all routes registered", func(t *testing.T) {
+	t.Run("All routes are registered when SignupEnabled is true, AppPasswordResetURL & AppPasswordlessTokenURL are not nil, and OAuth provider 'test' is configured", func(t *testing.T) {
 		gmux := runtime.NewServeMux()
 		router := mux.NewRouter()
 		app := test.App()
@@ -62,21 +71,35 @@ func TestRegisterRoutes(t *testing.T) {
 		app.OauthProviders["test"] = *testOAuthProvider
 		RegisterRoutes(router, app, gmux)
 
+		// to collect all the registered routes of *mux.Router
+		registered := make(map[string]bool)
+		expected := copyMap(pathMethods)
+
 		router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 			methods, err := route.GetMethods()
 			require.NoError(t, err)
 			pathtpl, err := route.GetPathTemplate()
 			require.NoError(t, err)
-			assert.True(t, pathMethods[fmt.Sprintf("%+v:%s", methods, pathtpl)])
+
+			registered[fmt.Sprintf("%+v:%s", methods, pathtpl)] = true
 			return nil
 		})
+		assert.EqualValues(t, expected, registered)
 	})
-	t.Run("App with SignupEnabled disabled doesn't have /accounts and /accounts/available routes registered", func(t *testing.T) {
+	t.Run("/accounts and /accounts/available routes are not registered when SignupEnabled is false", func(t *testing.T) {
 		gmux := runtime.NewServeMux()
 		router := mux.NewRouter()
 		app := test.App()
 		app.Config.EnableSignup = false
 		RegisterRoutes(router, app, gmux)
+
+		// to collect all the registered routes of *mux.Router
+		registered := make(map[string]bool)
+		expected := copyMap(pathMethods)
+
+		// These 2 routes shouldn't be registered when signup is disabled
+		delete(expected, "[POST]:/accounts")
+		delete(expected, "[GET]:/accounts/available")
 
 		router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 			methods, err := route.GetMethods()
@@ -86,15 +109,26 @@ func TestRegisterRoutes(t *testing.T) {
 			pathKey := fmt.Sprintf("%+v:%s", methods, pathtpl)
 			assert.NotEqual(t, "[POST]:/accounts", pathKey)
 			assert.NotEqual(t, "[GET]:/accounts/available", pathKey)
+
+			registered[pathKey] = true
 			return nil
 		})
 	})
-	t.Run("App with AppPasswordlessTokenURL nil doesn't have {GET,POST}:/session/token routes registered", func(t *testing.T) {
+	t.Run("{GET,POST}:/session/token routes are not registered when AppPasswordlessTokenURL is not configured", func(t *testing.T) {
 		gmux := runtime.NewServeMux()
 		router := mux.NewRouter()
 		app := test.App()
 		app.Config.AppPasswordlessTokenURL = nil
+		app.OauthProviders["test"] = *testOAuthProvider
 		RegisterRoutes(router, app, gmux)
+
+		// to collect all the registered routes of *mux.Router
+		registered := make(map[string]bool)
+		expected := copyMap(pathMethods)
+
+		// Passwordless authentication related routes
+		delete(expected, "[POST]:/session/token")
+		delete(expected, "[GET]:/session/token")
 
 		router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 			methods, err := route.GetMethods()
@@ -104,15 +138,26 @@ func TestRegisterRoutes(t *testing.T) {
 			pathKey := fmt.Sprintf("%+v:%s", methods, pathtpl)
 			assert.NotEqual(t, "[POST]:/session/token", pathKey)
 			assert.NotEqual(t, "[GET]:/session/token", pathKey)
+
+			registered[pathKey] = true
 			return nil
 		})
+		assert.EqualValues(t, expected, registered)
 	})
-	t.Run("App with AppPasswordResetURL nil doesn't have /password/reset route registered", func(t *testing.T) {
+	t.Run("/password/reset is not registered when AppPasswordResetURL is not configured", func(t *testing.T) {
 		gmux := runtime.NewServeMux()
 		router := mux.NewRouter()
 		app := test.App()
 		app.Config.AppPasswordResetURL = nil
+		app.OauthProviders["test"] = *testOAuthProvider
 		RegisterRoutes(router, app, gmux)
+
+		// to collect all the registered routes of *mux.Router
+		registered := make(map[string]bool)
+		expected := copyMap(pathMethods)
+
+		// Remove the route responsible for handling password reset requests
+		delete(expected, "[GET]:/password/reset")
 
 		router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 			methods, err := route.GetMethods()
@@ -121,15 +166,56 @@ func TestRegisterRoutes(t *testing.T) {
 			require.NoError(t, err)
 			pathKey := fmt.Sprintf("%+v:%s", methods, pathtpl)
 			assert.NotEqual(t, "[GET]:/password/reset", pathKey)
+
+			registered[pathKey] = true
 			return nil
 		})
+		assert.EqualValues(t, expected, registered)
 	})
-	t.Run("App with Actives nil doesn't have /stats route registered", func(t *testing.T) {
+	t.Run("/oauth prefixed routes are not registered when no OAuth providers are configured", func(t *testing.T) {
+		gmux := runtime.NewServeMux()
+		router := mux.NewRouter()
+		app := test.App()
+		require.Len(t, app.OauthProviders, 0)
+		RegisterRoutes(router, app, gmux)
+
+		// to collect all the registered routes of *mux.Router
+		registered := make(map[string]bool)
+		expected := copyMap(pathMethods)
+
+		for k := range expected {
+			if strings.Contains(k, "oauth") {
+				delete(expected, k)
+			}
+		}
+
+		router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+			methods, err := route.GetMethods()
+			require.NoError(t, err)
+			pathtpl, err := route.GetPathTemplate()
+			require.NoError(t, err)
+			assert.False(t, strings.HasPrefix(pathtpl, "/oauth"))
+
+			pathKey := fmt.Sprintf("%+v:%s", methods, pathtpl)
+			registered[pathKey] = true
+			return nil
+		})
+		assert.EqualValues(t, expected, registered)
+	})
+	t.Run("/stats route is not registered when App with Actives is nil", func(t *testing.T) {
 		gmux := runtime.NewServeMux()
 		router := mux.NewRouter()
 		app := test.App()
 		app.Actives = nil
+		app.OauthProviders["test"] = *testOAuthProvider
 		RegisterRoutes(router, app, gmux)
+
+		// to collect all the registered routes of *mux.Router
+		registered := make(map[string]bool)
+		expected := copyMap(pathMethods)
+
+		// Remove the route responsible for handling password reset requests
+		delete(expected, "[GET]:/stats")
 
 		router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 			methods, err := route.GetMethods()
@@ -138,7 +224,10 @@ func TestRegisterRoutes(t *testing.T) {
 			require.NoError(t, err)
 			pathKey := fmt.Sprintf("%+v:%s", methods, pathtpl)
 			assert.NotEqual(t, "[GET]:/stats", pathKey)
+
+			registered[pathKey] = true
 			return nil
 		})
+		assert.EqualValues(t, expected, registered)
 	})
 }
--- grpc/public/routing_test.go
+++ grpc/public/routing_test.go
@@ -2,6 +2,7 @@ package public
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/gorilla/mux"
@@ -33,6 +34,14 @@ var pathMethods = map[string]bool{
 	"[GET]:/oauth/test/return":  true,
 }
 
+func copyMap(src map[string]bool) map[string]bool {
+	dst := make(map[string]bool)
+	for k, v := range src {
+		dst[k] = v
+	}
+	return dst
+}
+
 func TestRegisterRoutes(t *testing.T) {
 	testOAuthProvider := oauth.NewProvider(&oauth2.Config{}, func(t *oauth2.Token) (*oauth.UserInfo, error) {
 		return &oauth.UserInfo{
@@ -40,7 +49,7 @@ func TestRegisterRoutes(t *testing.T) {
 			Email: "test@example.com",
 		}, nil
 	})
-	t.Run("App with SignupEnabled & AppPasswordResetURL enabled has all routes registered", func(t *testing.T) {
+	t.Run("All routes are registered when SignupEnabled is true, AppPasswordResetURL & AppPasswordlessTokenURL are not nil, and OAuth provider 'test' is configured", func(t *testing.T) {
 		gmux := runtime.NewServeMux()
 		router := mux.NewRouter()
 		app := test.App()
@@ -48,21 +57,36 @@ func TestRegisterRoutes(t *testing.T) {
 		app.OauthProviders["test"] = *testOAuthProvider
 		RegisterRoutes(router, app, gmux)
 
+		// to collect all the registered routes of *mux.Router
+		registered := make(map[string]bool)
+		expected := copyMap(pathMethods)
+
 		router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 			methods, err := route.GetMethods()
 			require.NoError(t, err)
 			pathtpl, err := route.GetPathTemplate()
 			require.NoError(t, err)
-			assert.True(t, pathMethods[fmt.Sprintf("%+v:%s", methods, pathtpl)])
+
+			registered[fmt.Sprintf("%+v:%s", methods, pathtpl)] = true
 			return nil
 		})
+		assert.EqualValues(t, expected, registered)
 	})
-	t.Run("App with SignupEnabled disabled doesn't /accounts and /accounts/available routes registered", func(t *testing.T) {
+	t.Run("/accounts and /accounts/available routes are not registered when SignupEnabled is false", func(t *testing.T) {
 		gmux := runtime.NewServeMux()
 		router := mux.NewRouter()
 		app := test.App()
 		app.Config.EnableSignup = false
+		app.OauthProviders["test"] = *testOAuthProvider
 		RegisterRoutes(router, app, gmux)
+
+		// to collect all the registered routes of *mux.Router
+		registered := make(map[string]bool)
+		expected := copyMap(pathMethods)
+
+		// These 2 routes shouldn't be registered when signup is disabled
+		delete(expected, "[POST]:/accounts")
+		delete(expected, "[GET]:/accounts/available")
 
 		router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 			methods, err := route.GetMethods()
@@ -72,15 +96,27 @@ func TestRegisterRoutes(t *testing.T) {
 			pathKey := fmt.Sprintf("%+v:%s", methods, pathtpl)
 			assert.NotEqual(t, "[POST]:/accounts", pathKey)
 			assert.NotEqual(t, "[GET]:/accounts/available", pathKey)
+
+			registered[pathKey] = true
 			return nil
 		})
+		assert.EqualValues(t, expected, registered)
 	})
-	t.Run("App with AppPasswordlessTokenURL nil doesn't {GET,POST}:/session/token routes registered", func(t *testing.T) {
+	t.Run("{GET,POST}:/session/token routes are not registered when AppPasswordlessTokenURL is not configured", func(t *testing.T) {
 		gmux := runtime.NewServeMux()
 		router := mux.NewRouter()
 		app := test.App()
 		app.Config.AppPasswordlessTokenURL = nil
+		app.OauthProviders["test"] = *testOAuthProvider
 		RegisterRoutes(router, app, gmux)
+
+		// to collect all the registered routes of *mux.Router
+		registered := make(map[string]bool)
+		expected := copyMap(pathMethods)
+
+		// Passwordless authentication related routes
+		delete(expected, "[POST]:/session/token")
+		delete(expected, "[GET]:/session/token")
 
 		router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 			methods, err := route.GetMethods()
@@ -90,15 +126,26 @@ func TestRegisterRoutes(t *testing.T) {
 			pathKey := fmt.Sprintf("%+v:%s", methods, pathtpl)
 			assert.NotEqual(t, "[POST]:/session/token", pathKey)
 			assert.NotEqual(t, "[GET]:/session/token", pathKey)
+
+			registered[pathKey] = true
 			return nil
 		})
+		assert.EqualValues(t, expected, registered)
 	})
-	t.Run("App with AppPasswordResetURL nil doesn't /password/reset route registered", func(t *testing.T) {
+	t.Run("/password/reset is not registered when AppPasswordResetURL is not configured", func(t *testing.T) {
 		gmux := runtime.NewServeMux()
 		router := mux.NewRouter()
 		app := test.App()
 		app.Config.AppPasswordResetURL = nil
+		app.OauthProviders["test"] = *testOAuthProvider
 		RegisterRoutes(router, app, gmux)
+
+		// to collect all the registered routes of *mux.Router
+		registered := make(map[string]bool)
+		expected := copyMap(pathMethods)
+
+		// Remove the route responsible for handling password reset requests
+		delete(expected, "[GET]:/password/reset")
 
 		router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
 			methods, err := route.GetMethods()
@@ -107,7 +154,40 @@ func TestRegisterRoutes(t *testing.T) {
 			require.NoError(t, err)
 			pathKey := fmt.Sprintf("%+v:%s", methods, pathtpl)
 			assert.NotEqual(t, "[GET]:/password/reset", pathKey)
+
+			registered[pathKey] = true
 			return nil
 		})
+		assert.EqualValues(t, expected, registered)
+	})
+	t.Run("/oauth prefixed routes are not registered when no OAuth providers are configured", func(t *testing.T) {
+		gmux := runtime.NewServeMux()
+		router := mux.NewRouter()
+		app := test.App()
+		require.Len(t, app.OauthProviders, 0)
+		RegisterRoutes(router, app, gmux)
+
+		// to collect all the registered routes of *mux.Router
+		registered := make(map[string]bool)
+		expected := copyMap(pathMethods)
+
+		for k := range expected {
+			if strings.Contains(k, "oauth") {
+				delete(expected, k)
+			}
+		}
+
+		router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+			methods, err := route.GetMethods()
+			require.NoError(t, err)
+			pathtpl, err := route.GetPathTemplate()
+			require.NoError(t, err)
+			assert.False(t, strings.HasPrefix(pathtpl, "/oauth"))
+
+			pathKey := fmt.Sprintf("%+v:%s", methods, pathtpl)
+			registered[pathKey] = true
+			return nil
+		})
+		assert.EqualValues(t, expected, registered)
 	})
 }
