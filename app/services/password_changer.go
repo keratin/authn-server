3d2b27b473269fefdb7baf2f0a83621a968d4664
Tanner Henhawke <tannerhenhawke@gmail.com>
2019-10-03 16:13:42 -0400

    Added MFA checks to handlers/post_password.go

--- app/services/password_changer.go
+++ app/services/password_changer.go
@@ -3,12 +3,14 @@ package services
 import (
 	"github.com/keratin/authn-server/app"
 	"github.com/keratin/authn-server/app/data"
+	"github.com/keratin/authn-server/lib/compat"
 	"github.com/keratin/authn-server/ops"
 	"github.com/pkg/errors"
+	"github.com/pquerna/otp/totp"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func PasswordChanger(store data.AccountStore, r ops.ErrorReporter, cfg *app.Config, id int, currentPassword string, password string) error {
+func PasswordChanger(store data.AccountStore, r ops.ErrorReporter, cfg *app.Config, id int, currentPassword string, password string, totpCode string) error {
 	account, err := store.Find(id)
 	if err != nil {
 		return errors.Wrap(err, "Find")
@@ -22,6 +24,17 @@ func PasswordChanger(store data.AccountStore, r ops.ErrorReporter, cfg *app.Conf
 	err = bcrypt.CompareHashAndPassword(account.Password, []byte(currentPassword))
 	if err != nil {
 		return FieldErrors{{"credentials", ErrFailed}}
+	}
+
+	//Check TOTP MFA
+	if account.TOTPEnabled() {
+		secret, err := compat.Decrypt([]byte(account.TOTPSecret.String), cfg.DBEncryptionKey)
+		if err != nil {
+			return errors.Wrap(err, "TOTPDecrypt")
+		}
+		if !totp.Validate(totpCode, secret) {
+			return FieldErrors{{"totp", ErrInvalidOrExpired}}
+		}
 	}
 
 	return PasswordSetter(store, r, cfg, id, password)
