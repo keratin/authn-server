08b0759615de3d7551ebe4f43951774a8a96e9af
Martin Reus <martinreus@gmail.com>
2020-02-08 21:58:52 +0000

    improving test readability

--- server/handlers/get_password_strength_test.go
+++ server/handlers/get_password_strength_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/keratin/authn-server/lib/route"
 	"github.com/keratin/authn-server/server/test"
 	"github.com/stretchr/testify/require"
+	"net/url"
 	"testing"
 )
 
@@ -16,23 +17,19 @@ func TestPostPasswordScore(t *testing.T) {
 
 	client := route.NewClient(server.URL).Referred(&app.Config.ApplicationDomains[0])
 
-	testCases := []struct {
-		Description string
-		Password     string
-		Result      map[string]interface{} // expected JSON response
-	}{
-		{Description: "Should return score 1", Password: "strongerPassword", Result: map[string]interface{}{"score": 1, "requiredScore": 2}},
-		{Description: "Should return score 3", Password: "strongerPassword17", Result: map[string]interface{}{"score": 3, "requiredScore": 2}},
-	}
+	t.Run("Should successfully score password using JSON request", func(t *testing.T) {
+		res, err := client.PostJSON("/password/score", map[string]interface{}{"password": "aSmallPassword"})
 
-	for _, testCase := range testCases {
-		t.Run(testCase.Description, func(t *testing.T) {
-			res, err := client.PostJSON("/password/score", map[string]interface{}{"password": testCase.Password})
+		require.NoError(t, err)
+		test.AssertData(t, res, map[string]interface{}{"score": 3, "requiredScore": 2})
+	})
 
-			require.NoError(t, err)
-			test.AssertData(t, res, testCase.Result)
-		})
-	}
+	t.Run("Should successfully score password using JSON request", func(t *testing.T) {
+		res, err := client.PostForm("/password/score", url.Values{"password": []string{"anotherBetterPassword!"}})
+
+		require.NoError(t, err)
+		test.AssertData(t, res, map[string]interface{}{"score": 4, "requiredScore": 2})
+	})
 
 	t.Run("Should accuse missing password", func(t *testing.T) {
 		res, err := client.PostJSON("/password/score?password=", map[string]interface{}{})
@@ -43,5 +40,6 @@ func TestPostPasswordScore(t *testing.T) {
 			Message: services.ErrMissing,
 		}})
 	})
+
 }
 
